3cddfad216dd14ffdf9f201dc05468b11c348230
suijiafeng <suijiafeng@hotmail.com>
2022-04-03 16:41:32 +0800

    feat: 引入了 elementui 构建了简单的页面

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
-import './style2.scss'
+import store from './store'
+import './style'
+import './components'
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
